3d8b6b6818dd6986f029d9453b198ec5395de990
daniel.meshorer <danielmeshorer@gmail.com>
2023-02-05 12:41:07 +0200

    update ex1 in signals

--- system_programming/signals/signals.c
+++ system_programming/signals/signals.c
@@ -1,72 +1,97 @@
-#define _POSIX_SOURCE           /* resolve inplicit dec of kill */
-#define _XOPEN_SOURCE   600     /* esolve inplicit dec of usleep*/
-#include <unistd.h>             /* fork , execvp */
+#define _POSIX_SOURCE           /* resolve implicit declaration of kill */
+#define _XOPEN_SOURCE   600     /* resolve implicit declaration of usleep */
+#include <unistd.h>             /* fork, execvp */
 #include <stdio.h>
 #include <signal.h>
 #include <sys/types.h>          /* pid_t */
+#include <errno.h>
 
-/*
-    using fork:
-    Send signal SIGUSR1 from parent to child.
-    When the child receives SIGUSR1 it should return signal SIGUSR2 to its parent, and vice versa.
-*/
+#define WORD_LENGTH 5
 
-volatile sig_atomic_t childglobal = 1;
-volatile sig_atomic_t parentglobal = 1;
-
-void handle_child(int sig)
+void sig_hand_child(int sig)                                /* signal handle function for child */
 {
     if (sig == SIGUSR1)
     {
-        parentglobal--;
-        write(STDOUT_FILENO,"ping\n",5);
+        write(STDOUT_FILENO, "ping\n", WORD_LENGTH);        /* write is an async-signal-safe function */
     }
 }
 
-void handle_parent(int sig)
+void sig_hand_parent(int sig)                               /* signal handle function for parent */
 {
     if (sig == SIGUSR2)
     {
-        childglobal--;
-        write(STDOUT_FILENO,"pong\n",5);
+        write(STDOUT_FILENO, "pong\n", WORD_LENGTH);
     }
+}
+
+
+int child_process()
+{
+	while(1)
+	{
+        pause();                                            /* wait for signal */
+		
+		usleep(1000000);                                    /* sleep for 1 second */
+
+		if (-1 == kill(getppid(), SIGUSR2))                 /* send SIGUSR2 signal to parent procesig_handparentss */
+        {
+            return errno;
+        }                         
+	}
+	return 0;
+}
+
+int parent_process(pid_t pid)
+{
+	while (1)
+	{
+		usleep(1000000);                                    /* sleep for 1 second */
+
+		if (-1 == kill(pid, SIGUSR1))                       /* send SIGUSR1 signal to child process (pid recieved as argument) */
+        {
+            return errno;
+        }                                 
+        pause();                                            /* wait for signal */
+	}
+	return 0;
 }
 
 int main()
 {
+	
+	pid_t child_pid = 0;
+	struct sigaction sa_parent;
+    struct sigaction sa_child;
+    
+    
+	sa_child.sa_handler = sig_hand_child;                   /* set up signal handler function for child */
+    sa_parent.sa_handler = sig_hand_parent;                 /* set up signal handler function for parent */
 
-    pid_t pid_child = 0;
-   
-    signal(SIGUSR1, handle_child);
-    signal(SIGUSR2, handle_parent);
+	if (sigaction(SIGUSR1, &sa_child, NULL) == -1)
+	{
+		perror("sigaction");
+	}
 
-    pid_child = fork();
-   
-    if (pid_child < 0)
+    if (sigaction(SIGUSR2, &sa_parent, NULL) == -1)
+	{
+		perror("sigaction");
+	}
+	
+	child_pid = fork();                                     /* create child process */
+
+    if (-1 == child_pid)                                    /* check that fork worked */
     {
-        perror("fork error");
-        return pid_child;
-    }
-    if (0 == pid_child)    
-    {
-        while (childglobal)
-        {
-             raise(SIGUSR2);
-             pause();
-              
-        }
-       
-    }
-    else                   
-    {
-        while (parentglobal)
-        {   
-            kill(pid_child, SIGUSR1);
-            pause();     
-        }
-        
+        perror("fork");
     }
 
-    return 0;
+	if (0 == child_pid)
+	{
+		child_process();                                    /* run child_process()  */
+	}
+	else
+	{
+		parent_process(child_pid);                          /* run parent_process()  */
+	}
+	
+	return 0;
 }
-
