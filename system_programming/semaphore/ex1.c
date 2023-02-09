29682629313e245582e8b9f38a30b0524d8bb71f
daniel.meshorer <danielmeshorer@gmail.com>
2023-02-09 16:03:54 +0200

    update

--- system_programming/semaphore/ex1.c
+++ system_programming/semaphore/ex1.c
@@ -5,12 +5,35 @@
 #include <stdlib.h> /*atoi */
 #include <string.h> /*strcmp*/
 
+
+int CheckUndo(char *command)
+{
+    int count_space = 0;
+    while(*command)
+    {
+        if (*command == ' ')
+        {
+            count_space++;
+        }
+        command++;
+        if (count_space == 2)
+        {
+            break;
+        }
+    }
+    if (strncmp(command, "undo", 4) == 0)
+    {
+        return 0;
+    }
+    return 1;
+}
+
 int Decrement(sem_t *sem, int number)
 {
     int i = 0;
-    for (i == 0; i < number; i++)
+    for (i = 0; i < number; i++)
     {
-        if (-1 == sem_trywait(sem))
+        if (-1 == sem_wait(sem))
         {
             return -1;
         }
@@ -21,7 +44,7 @@ int Decrement(sem_t *sem, int number)
 int Increment(sem_t *sem, int number)
 {
     int i = 0;
-    for (i == 0; i < number; i++)
+    for (i = 0; i < number; i++)
     {
         if (-1 == sem_post(sem))
         {
@@ -46,7 +69,12 @@ int main(int argc, char **argv)
     sem_t *sem = NULL;
     int number = 0;
     int result = 0;
-    sem = sem_open(name_sem, O_CREAT, O_EXCL, 1);
+    int counter_undo = 0;
+    if (2 > argc)
+    {
+        return -1;
+    }
+    sem = sem_open(name_sem, O_CREAT, 0666, 1);
     if (sem == SEM_FAILED)
     {
         perror("sem_open");
@@ -55,14 +83,24 @@ int main(int argc, char **argv)
     while (1)
     {
         printf("enter a command on the semaphore\n");
-        fgets(command, sizeof(command) - 1, stdin);
+        if (NULL == fgets(command, sizeof(command) - 1, stdin))
+        {
+            perror("fgets");
+            continue;
+        }
+        command[strlen(command) - 1] = '\0';
+        
 
         if ((*command == 'D') | (*command == 'I'))
         {
             number = atoi(command + 2);
-
+           
             if (*command == 'D')
             {
+                if ( 0 == CheckUndo(command))
+                {
+                    counter_undo-=number;
+                }
                 result = Decrement(sem, number);
                 if (-1 == result)
                 {
@@ -70,7 +108,11 @@ int main(int argc, char **argv)
                 }
             }
             else
-            {
+            {  
+                if ( 0 == CheckUndo(command))
+                {
+                    counter_undo+=number;
+                }
                 result = Increment(sem, number);
                 if (-1 == result)
                 {
@@ -78,7 +120,7 @@ int main(int argc, char **argv)
                 }
             }
         }
-        if (0 == strncmp(command, "V", 1))
+        if (0 == strcmp(command, "V"))
         {
             result = ViewSem(sem);
             if (-1 == result)
@@ -91,10 +133,42 @@ int main(int argc, char **argv)
             }
         }
 
-       if (0 == strncmp(command, "X", 1))
+       if (0 == strcmp(command, "X"))
         {
-            sem_close(sem);
-            sem_unlink(name_sem);
+            if (0 > counter_undo)
+            {
+                result = Increment(sem, counter_undo);
+                if (-1 == result)
+                {
+                    perror("Increment");
+                }
+            }
+             if (0 < counter_undo)
+            {
+                result = Decrement(sem, counter_undo);
+                if (-1 == result)
+                {
+                    perror("Decrement");
+                }
+            }
+            if ( -1 == sem_close(sem))
+            {
+                perror("sem_close");
+            }
+            break;
+        }
+        if (0 == strcmp(command, "Q"))
+        {
+            
+            if ( -1 == sem_close(sem))
+            {
+                perror("sem_close");
+            }
+            if ( -1 == sem_unlink(name_sem))
+            {
+                perror("Sem unlink");
+            }
+            
             break;
         }
     }
