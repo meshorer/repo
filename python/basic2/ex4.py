36a75bc6ee143431570d35289725a2456631320f
daniel.meshorer <danielmeshorer@gmail.com>
2023-02-16 18:59:28 +0200

    update

--- python/basic2/ex4.py
+++ python/basic2/ex4.py
@@ -4,9 +4,7 @@ a = ['a','b',3]
 print(ex4_list(a))
 
 
-def ex4_dict(my_list):
-    return list(enumerate(my_list))
-a = {'d': 1, 'a': 2, 'n': 2, 'i': 1, ' ': 1, 'm': 1}
-print(ex4_dict(a))
 
-
+def ex4_dict2(my_list):
+    return dict(enumerate(my_list.items()))
+print(ex4_dict2(a))
--- python/basic2/ex6.py
+++ python/basic2/ex6.py
@@ -1,14 +1,5 @@
 def left_rotate(lst, n):
-    """
-    Rotate the list `lst` to the left `n` times using slicing.
-
-    Args:
-    lst: A list to rotate.
-    n: An integer indicating how many times to rotate the list.
-
-    Returns:
-    The rotated list.
-    """
-    return lst[n:] + lst[:n]
+    return lst[n:] + lst[:n] # cut the n elements of the list, and add them at the end of the list
 a = [1,2,3,4,5,6]
-print(left_rotate(a,2))
+print(left_rotate(a,1))
+print(locals())
