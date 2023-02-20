02f6cacf147f34fb7d661bcb35661fc2350b7350
daniel.meshorer <danielmeshorer@gmail.com>
2023-02-21 00:56:49 +0200

    update

--- python/oop_attributes/ex2.py
+++ python/oop_attributes/ex2.py
@@ -1,4 +1,4 @@
-class  X(object):
+class  X(object):               # with __setattr__
     def __init__(self, a):
         object.__setattr__(self, 'a', a)
 
@@ -7,22 +7,22 @@ class  X(object):
             raise AttributeError(f"attribute '{attr}' doesn't exist")
         object.__setattr__(self, attr, value)
 
-
-
-
-
-
 my_class = X(4)
 print(my_class.a)
 my_class.a = 3
 my_class.a = 5
 print(my_class.a)
 
+
+
+class Y(object):                    # with __slots__
+    __slots__ = ['a']
+    def __init__(self, a):
+        self.a = a
+my_class = Y(4)
+print(my_class.a)
+my_class.a = 3
+my_class.a = 5
+print(my_class.a)
+
 my_class.b = 8
-
-
-
-#if attr not in self.__dict__:
-#            raise AttributeError(f"attribute '{attr}' doesn't exist")
-
-# object.__setattr__(self, attr, value)
