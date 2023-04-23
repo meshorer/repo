a6b5be31f10becc31f08f3ef3c281e0e16664e99
daniel.meshorer <danielmeshorer@gmail.com>
2023-04-23 22:35:18 +0300

    update

--- cyber/c2/client_icmp_c2.py
+++ cyber/c2/client_icmp_c2.py
@@ -2,6 +2,7 @@ import os
 from scapy.all import *
 
 def pkt_send(source,dest,data):
+    conf.L3socket = L3RawSocket
     sendp(IP(src=source,dst=dest)/ICMP()/data)
 
 def sniff_pkt():
@@ -13,7 +14,6 @@ def parse_packet(packet):
         data_recieved = packet[Raw].load
     print(data_recieved)
 
-  
-#pkt_send("10.1.0.104","10.1.0.173","yarin_believe in us")
-sniff_pkt()
+while 1:  
+    pkt_send("127.0.0.1","127.0.0.1","check data")
     
