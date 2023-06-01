79f0c0ff12eaebea922e9e68b2fcde72f46caf7b
daniel.meshorer <danielmeshorer@gmail.com>
2023-06-01 09:15:30 +0300

    update soc

--- cyber/soc/ex1.py
+++ cyber/soc/ex1.py
@@ -2,26 +2,41 @@ import syslog
 import socket
 import logging
 import logging.handlers
+from logging.handlers import SysLogHandler
 
-PORT_NUMBER = 514
+# PORT_NUMBER = 514
 
 
 def main():
-    my_logger = logging.getLogger(__name__)   # the name of the file
-    my_logger.setLevel(logging.CRITICAL)
-    # handler = logging.handlers.SysLogHandler(address=('localhost', 514),socktype= socket.SOCK_STREAM)
-    my_logger.addHandler(handler)
-    # forwarder = logging.handlers.SysLogHandler(address='/var/log/syslog')
-    # # my_logger.addHandler(forwarder)
-    #tests:
-    my_logger.debug('this is debug')
-    my_logger.critical('this is critical')
-    my_logger.warning('this is warning')
-    syslog.syslog('Processing started')
-    syslog.syslog(syslog.LOG_WARNING, "Test message at warning priority")
 
+    local_logger = logging.getLogger('MyLogger')
+    local_logger.setLevel(logging.DEBUG)
+
+    local_handler = logging.handlers.SysLogHandler(address = '/dev/log')
+
+    local_logger.addHandler(local_handler)
+
+    local_logger.debug('second debug')
+    local_logger.critical('second critical')
+
+
+
+    forwarder_logger = logging.getLogger(__name__)   # the name of the file
+    forwarder_logger.setLevel(logging.CRITICAL)
+    forwarder_handler = logging.handlers.SysLogHandler(address=('localhost', 514),socktype= socket.SOCK_STREAM)
+    forwarder_logger.addHandler(forwarder_handler)
+
+
+    forwarder_logger.debug('second debug')
+    forwarder_logger.critical('second critical')
+    forwarder_logger.warning('second warning')
+    # syslog.syslog('Processing started')
+    # syslog.syslog(syslog.LOG_WARNING, "Test message at warning priority")
 
 
 
 if __name__ == "__main__":
     main()
+
+
+
